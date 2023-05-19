6d0fed0acd03081168b5883795aea81aa61b4751
Isaque Dantas <116102632+IsqDantas@users.noreply.github.com>
2023-05-19 15:55:36 -0300

    Update q4.py

--- Lista - Dicionários e Funções/q4.py
+++ Lista - Dicionários e Funções/q4.py
@@ -1,10 +1,22 @@
-def funcao():
-    pass
+# Não entendi bem o que significa somar
+# pessoas, mas vou imaginar que isso
+# é unir os elementos de cada dicionário
+
+pessoas = [
+    ({'nome': 'Maria',
+    'cidade': 'Belo Horizonte'}),
+
+    ({'nome': 'Maria',
+    'cidade': 'São Paulo'}),
+
+    ({'nome': 'Pedro',
+    'cidade': 'Curitiba'})
+]
+
+for i in range(len(pessoas)):
+    chave = '-'.join(pessoas[i].keys())
+    valor = '-'.join(pessoas[i].values())
+    pessoas[i] = {chave: valor}
 
 
-def main():
-    pass
-
-
-if __name__ == '__main__':
-    main()
+print(pessoas)
