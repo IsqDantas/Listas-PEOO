214c9aac185f77e24541fdf57acca78a1b3e0f82
Isaque Dantas <116102632+IsqDantas@users.noreply.github.com>
2023-05-19 15:56:53 -0300

    Update q5.py

--- Lista - Dicionários e Funções/q5.py
+++ Lista - Dicionários e Funções/q5.py
@@ -1,9 +1,18 @@
-def funcao():
-    pass
+def multiplicar_dict(dicionario: dict, fator: float):
+    for chave, valor in dicionario.items():
+        dicionario[chave] = valor * fator
+    
+    return dicionario
 
 
 def main():
-    pass
+    salarios = {
+        'Isaque': 400,
+        'Davi': 0,
+        'Karolayne': 9999
+    }
+
+    print(multiplicar_dict(salarios, -1))
 
 
 if __name__ == '__main__':
