144738caa1af28348d5d5f2af2cc0d7a163cad52
Isaque Dantas <116102632+IsqDantas@users.noreply.github.com>
2023-05-19 15:57:37 -0300

    Update q7.py

--- Lista - Dicionários e Funções/q7.py
+++ Lista - Dicionários e Funções/q7.py
@@ -1,9 +1,50 @@
-def funcao():
-    pass
+# Há três possibilidades nesse caso: o dicionário
+# pode estar ordenado - e, então, crescentemente
+# ou decrescentemente - ou não ordenado.
+
+def order_in_dict(dicionario: dict):
+    valores = list(dicionario.values())
+    valores.sort(reverse=False)
+    ascendente = valores
+    
+    valores = list(dicionario.values())
+    valores.sort(reverse=True)
+    decrescente = valores
+
+    valores = list(dicionario.values())
+
+    if valores == ascendente:
+        return 'ascendente'
+    elif valores == decrescente:
+        return 'decrescente'
+    else:
+        return None
 
 
 def main():
-    pass
+    salarios = {
+        'Isaque': 400,
+        'Davi': 0,
+        'Karolayne': 99999
+    }
+
+    assert order_in_dict(salarios) == None
+    
+    salarios = {
+        'Davi': -99999,
+        'Isaque': 400,
+        'Karolayne': 99999
+    }
+
+    assert order_in_dict(salarios) == 'ascendente'
+    
+    salarios = {
+        'Karolayne': 99999,
+        'Isaque': 400,
+        'Davi': -99999
+    }
+
+    assert order_in_dict(salarios) == 'decrescente'
 
 
 if __name__ == '__main__':
